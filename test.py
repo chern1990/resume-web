05cdd64a1349f59c27144aee1c6e9c06944e8b16
chern1990 <chern1990@gmail.com>
2023-10-23 22:12:41 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+
+import asyncio, json
+
+async def serve_pdf(request: Request, contact:bool=False):
+    hostname = 'http://yeechern.ddns.net'
+    filename = 'Resume'
+    pdf_filepath = f'{filename}.pdf'
+
+    if not os.path.exists(pdf_filepath):
+        print('Generate PDF')
+        pdf = await pyppdf.main(
+                output_file = pdf_filepath,
+                url = hostname,
+                # html = html_src,
+                args={'pdf':{'format':'A4',
+                                    'printBackground':True,
+                                    'margin':{'top':'0.5in',
+                                                'right':'0.0in',
+                                                'bottom':'0.5in',
+                                                'left':'0.0in'}
+                                    },
+                      'launch':{'args':[
+                                        '--no-sandbox',                                        
+                                        '--single-process',
+                                        '--disable-dev-shm-usage',
+                                        '--disable-gpu',
+                                        '--no-zygote'
+                                ]}
+                      }
+                ) 
+    
