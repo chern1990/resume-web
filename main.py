3859ac366317f3e73c72988b265bd5db1c749743
chern1990 <chern1990@gmail.com>
2023-10-27 23:44:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,41 +22,41 @@ async def index(request: Request, contact:bool=False):
     return templates.TemplateResponse("resume.html", context={'request':request, **vars, 'contact':contact})
 
 
-@app.get("/pdf")
-async def serve_pdf(request: Request, contact:bool=False):
-    hostname = 'http://yeechern.ddns.net'
-    filename = 'Resume'
-    pdf_filepath = f'{filename}.pdf'
+# @app.get("/pdf")
+# async def serve_pdf(request: Request, contact:bool=False):
+#     hostname = 'http://yeechern.ddns.net'
+#     filename = 'Resume'
+#     pdf_filepath = f'{filename}.pdf'
 
-    vars = json.load(open("vars.json"))
+#     vars = json.load(open("vars.json"))
 
-    if not os.path.exists(pdf_filepath):
-        print('Generate PDF')
-        pdf = await pyppdf.main(
-                output_file = pdf_filepath,
-                url = hostname,
-                # html = html_src,
-                args={'pdf':{'format':'A4',
-                                    'printBackground':True,
-                                    'margin':{'top':'0.5in',
-                                                'right':'0.0in',
-                                                'bottom':'0.5in',
-                                                'left':'0.0in'}
-                                    },
-                      'launch':{'args':[
-                                        '--no-sandbox',                                        
-                                        '--single-process',
-                                        '--disable-dev-shm-usage',
-                                        '--disable-gpu',
-                                        '--no-zygote'
-                                ]}
-                      }
-                ) 
+#     if not os.path.exists(pdf_filepath):
+#         print('Generate PDF')
+#         pdf = await pyppdf.main(
+#                 output_file = pdf_filepath,
+#                 url = hostname,
+#                 # html = html_src,
+#                 args={'pdf':{'format':'A4',
+#                                     'printBackground':True,
+#                                     'margin':{'top':'0.5in',
+#                                                 'right':'0.0in',
+#                                                 'bottom':'0.5in',
+#                                                 'left':'0.0in'}
+#                                     },
+#                       'launch':{'args':[
+#                                         '--no-sandbox',                                        
+#                                         '--single-process',
+#                                         '--disable-dev-shm-usage',
+#                                         '--disable-gpu',
+#                                         '--no-zygote'
+#                                 ]}
+#                       }
+#                 ) 
     
-    dt = datetime.datetime.now().strftime('%Y%m%d')
-    try:
-        pdf_file = FileResponse(path=pdf_filepath, filename=f'{vars["name"]}_{dt}.pdf')
-    except:
-        pdf_file = 'file not found'
+#     dt = datetime.datetime.now().strftime('%Y%m%d')
+#     try:
+#         pdf_file = FileResponse(path=pdf_filepath, filename=f'{vars["name"]}_{dt}.pdf')
+#     except:
+#         pdf_file = 'file not found'
     
-    return pdf_file
+#     return pdf_file
