2f1ce122b92872c5d45052b35d4a17cb24d9cf8a
chern1990 <chern1990@gmail.com>
2023-10-27 08:32:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from pyppeteer import launch
 import asyncio
 async def main():
-    browser = await launch(options={'devtools': True, 'headless': False})
+    browser = await launch(executablePath='/usr/bin/google-chrome-stable', 
+                           options={'devtools': True, 'headless': False}, 
+                           args=['--no-sandbox'])
     page = await browser.newPage()
-    await page.goto('http://example.com')
-    await page.screenshot({'path': 'baidu.png'})
+    await page.goto('http://google.com')
+    await page.screenshot({'path': 'test.png'})
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
