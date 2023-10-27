6a57b139ff5dc0d948e7c6b314192c0f8a4cdf40
chern1990 <chern1990@gmail.com>
2023-10-28 00:24:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,19 +24,22 @@ async def index(request: Request, contact:bool=False):
 
 
 @app.get("/pdf")
-async def serve_pdf(request: Request, contact:bool=False):
-    hostname = 'http://yeechern.ddns.net'
-    filename = 'Resume'
-    pdf_filepath = f'{filename}.pdf'
+async def serve_pdf(request: Request):
+    subprocess.run(['wkhtmltopdf','http://yeechern.ddns.net','Resume.pdf'])
+    # hostname = 'http://yeechern.ddns.net'
+    # filename = 'Resume'
+    # pdf_filepath = f'{filename}.pdf'
 
-    options = {
-    'page-size': 'A4',
-    'margin-top': '0.5in',
-    'margin-right': '0.0in',
-    'margin-bottom': '0.5in',
-    'margin-left': '0.0in'
-    }
-    pdf = await pdfkit.from_url('http://yeechern.ddns.net', pdf_filepath, options=options)
+    # options = {
+    # 'page-size': 'A4',
+    # 'width':,
+    # 'margin-top': '0.5in',
+    # 'margin-right': '0.0in',
+    # 'margin-bottom': '0.5in',
+    # 'margin-left': '0.0in'
+    # }
+    # pdf = pdfkit.from_url('http://yeechern.ddns.net', pdf_filepath, options=options)
+    return 'done'
     
     # vars = json.load(open("vars.json"))
 #     if not os.path.exists(pdf_filepath):
