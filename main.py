52d85f5beaa379acda92ae49a8df84ce89eaefe7
chern1990 <chern1990@gmail.com>
2023-10-27 23:57:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import subprocess
 import datetime, time
 import os
 import urllib.request
+import pdfkit
 
 
 app = FastAPI()
@@ -22,14 +23,22 @@ async def index(request: Request, contact:bool=False):
     return templates.TemplateResponse("resume.html", context={'request':request, **vars, 'contact':contact})
 
 
-# @app.get("/pdf")
-# async def serve_pdf(request: Request, contact:bool=False):
-#     hostname = 'http://yeechern.ddns.net'
-#     filename = 'Resume'
-#     pdf_filepath = f'{filename}.pdf'
+@app.get("/pdf")
+async def serve_pdf(request: Request, contact:bool=False):
+    hostname = 'http://yeechern.ddns.net'
+    filename = 'Resume'
+    pdf_filepath = f'{filename}.pdf'
 
-#     vars = json.load(open("vars.json"))
-
+    options = {
+    'page-size': 'A4',
+    'margin-top': '0.5in',
+    'margin-right': '0.0in',
+    'margin-bottom': '0.5in',
+    'margin-left': '0.0in'
+    }
+    pdfkit.from_url('http://yeechern.ddns.net', pdf_filepath, options=options)
+    
+    vars = json.load(open("vars.json"))
 #     if not os.path.exists(pdf_filepath):
 #         print('Generate PDF')
 #         pdf = await pyppdf.main(
@@ -53,10 +62,10 @@ async def index(request: Request, contact:bool=False):
 #                       }
 #                 ) 
     
-#     dt = datetime.datetime.now().strftime('%Y%m%d')
-#     try:
-#         pdf_file = FileResponse(path=pdf_filepath, filename=f'{vars["name"]}_{dt}.pdf')
-#     except:
-#         pdf_file = 'file not found'
+    dt = datetime.datetime.now().strftime('%Y%m%d')
+    try:
+        pdf_file = FileResponse(path=pdf_filepath, filename=f'{vars["name"]}_{dt}.pdf')
+    except:
+        pdf_file = 'file not found'
     
-#     return pdf_file
+    return pdf_file
